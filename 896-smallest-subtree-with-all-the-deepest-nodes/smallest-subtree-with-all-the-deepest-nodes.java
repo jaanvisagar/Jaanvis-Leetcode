21bae5708a747dc33fda4cc1f325f0fa3765fd50
jaanvisagar <jaanvisagar125@gmail.com>
2026-01-09 23:18:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (18.16%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    class Result {
+        TreeNode node;
+        int dist;
+        Result(TreeNode n, int d) {
+            node = n;
+            dist = d;
+        }
+    }
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode node) {
+        if (node == null) return new Result(null, 0);
+
+        Result left = dfs(node.left);
+        Result right = dfs(node.right);
+
+        if (left.dist > right.dist) {
+            return new Result(left.node, left.dist + 1);
+        } 
+        else if (right.dist > left.dist) {
+            return new Result(right.node, right.dist + 1);
+        } 
+        else {
+            // Depths are equal; current node is the LCA
+            return new Result(node, left.dist + 1);
+        }
+    }
+}
